fe861775c8ee9e53f7583c8c3a4d0a645ebe86e1
du_xiaodong <1033784386@qq.com>
2019-08-15 14:26:26 +0800

    原型模式的分析与实践

---
+++ app/src/main/java/com/study/duxiaodong/designpattern_java/DesignPattern/Creational/PrototypePattern/Case/PrototypePatternTest.java
@@ -0,0 +1,28 @@
+package com.study.duxiaodong.designpattern_java.DesignPattern.Creational.PrototypePattern.Case;
+
+/**
+ * author : gavin_du
+ * email : 1033784386@qq.com
+ * date : 2019/8/15 14:11
+ * description : 原型模式
+ */
+public class PrototypePatternTest {
+    public static void main(String[] args) {
+        Student student1 = new Student("张三",new School("东阳一中","吴宁东路5号"));
+
+        Student student2 = student1.clone();
+        student2.setName("李四");
+        student2.setSchool(new School("东阳二中","吴宁西路8号"));
+
+        Student student3 = student1.clone();
+        student3.setName("王五");
+        student3.setSchool(new School("东阳三中","吴宁西路100号"));
+
+        System.out.println(student1);;
+
+        System.out.println(student2);
+
+        System.out.println(student3);
+
+    }
+}
